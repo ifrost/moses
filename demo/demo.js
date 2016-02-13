e036977aa8351bed749d51783c266cc0768764d4
Piotr Jamroz <pm.jamroz@gmail.com>
2016-02-13 16:04:04 +0000

    Add a simple view to visualise the sampler

--- demo/demo.js
+++ demo/demo.js
@@ -1,25 +1,22 @@
 define(function(require) {
     var p = require('p'),
-        DistanceSampler = require('sampler/distance-sampler');
+        SamplerDemo = require('demo/view/sampler-demo');
 
     var Demo = p.extend({
 
         sampler: null,
 
-        run: function(element) {
-            this.sampler = DistanceSampler.create(element, 50);
-            this.sampler.on("started", this._log.bind(this, 'started'), this);
-            this.sampler.on("sampled", this._log.bind(this, 'sampled'), this);
-            this.sampler.on("finished", this._log.bind(this, 'finished'), this);
-            this.sampler.on("activated", this._log.bind(this, 'activated'), this);
-            this.sampler.on("deactivated", this._log.bind(this, 'deactivated'), this);
-
-            this.sampler.activate();
+        $create: function() {
+            this._context = p.Context.create();
+            this.appView = p.Component.Root(document.body, this._context);
+            this._context.build();
         },
 
-        _log: function(event) {
-            console.log(event + ': ' + this.sampler.getData().length + ' points. Last point: ' + this.sampler.getLastPosition());
+        run: function(element) {
+            this.samplerDemo = SamplerDemo.create();
+            this.appView.add(this.samplerDemo);
         }
+
     });
 
     return Demo;
---
+++ demo/view/sampler-demo.js
@@ -0,0 +1,99 @@
+define(function(require){
+
+    var p = require('p'),
+        DistanceSampler = require('sampler/distance-sampler');
+
+    var SamplerDemo = p.Component.extend({
+
+        tag: 'div',
+
+        $create: function() {
+            this.root.id = "playground";
+            this.sampler = DistanceSampler.create(this.root, 20);
+            this.sampler.on("started", this._log.bind(this, 'started'), this);
+            this.sampler.on("sampled", this._log.bind(this, 'sampled'), this);
+            this.sampler.on("finished", this._log.bind(this, 'finished'), this);
+            this.sampler.on("activated", this._log.bind(this, 'activated'), this);
+            this.sampler.on("deactivated", this._log.bind(this, 'deactivated'), this);
+
+            this.sampler.on("started", this._draw.bind(this), this);
+            this.sampler.on("sampled", this._draw.bind(this), this);
+
+            this.sampler.on("finished", this._clear.bind(this), this);
+        },
+
+        init: function() {
+            this.sampler.activate();
+        },
+
+        _draw: function() {
+
+            var position = this.sampler.getLastPosition();
+
+            var marker = document.createElement('div');
+            marker.style.width = '4px';
+            marker.style.height = '4px';
+            marker.style.backgroundColor = 'black';
+            marker.style.position = 'absolute';
+            marker.style.top = (position.y - 1) + 'px';
+            marker.style.left = (position.x - 1) + 'px';
+            this.root.appendChild(marker);
+
+            if (!this._previousPoint) {
+                this._previousPoint = position;
+                return;
+            }
+
+            var line = createLine(this._previousPoint.x, this._previousPoint.y, position.x, position.y)
+            this.root.appendChild(line);
+            this._previousPoint = position;
+        },
+
+        _clear: function() {
+            this._previousPoint = null;
+            setTimeout(function(){
+                this.root.innerHTML = '';
+            }.bind(this), 1000)
+        },
+
+        _log: function(event) {
+            console.log(event + ': ' + this.sampler.getData().length + ' points. Last point: ' + this.sampler.getLastPosition());
+        }
+
+    });
+
+    return SamplerDemo;
+});
+
+
+function createLineElement(x, y, length, angle) {
+    var line = document.createElement("div");
+    var styles = 'border: 1px solid black; '
+        + 'width: ' + length + 'px; '
+        + 'height: 0px; '
+        + '-moz-transform: rotate(' + angle + 'rad); '
+        + '-webkit-transform: rotate(' + angle + 'rad); '
+        + '-o-transform: rotate(' + angle + 'rad); '
+        + '-ms-transform: rotate(' + angle + 'rad); '
+        + 'position: absolute; '
+        + 'top: ' + y + 'px; '
+        + 'left: ' + x + 'px; ';
+    line.setAttribute('style', styles);
+    return line;
+}
+
+function createLine(x1, y1, x2, y2) {
+    var a = x1 - x2,
+        b = y1 - y2,
+        c = Math.sqrt(a * a + b * b);
+
+    var sx = (x1 + x2) / 2,
+        sy = (y1 + y2) / 2;
+
+    var x = sx - c / 2,
+        y = sy;
+
+    var alpha = Math.PI - Math.atan2(-b, a);
+
+    return createLineElement(x, y, c, alpha);
+}
