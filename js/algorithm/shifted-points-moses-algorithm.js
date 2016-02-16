583d356a3644600f16864e71c2ac12d7492af1c5
Piotr Jamroz <pm.jamroz@gmail.com>
2016-02-16 22:08:40 +0000

    Minor bugfixes

--- js/algorithm/shifted-points-moses-algorithm.js
+++ js/algorithm/shifted-points-moses-algorithm.js
@@ -4,41 +4,40 @@ define(function(require){
    
    var ShiftedPointsMosesAlgorithm = DefaultMosesAlgorithm.extend({
        
-       preparePatternData: function(data) {
+       _preparePatternData: function(data) {
            return this._shiftData(data);
        },
        
-       prepareSamplingData: function(data) {
+       _prepareSamplingData: function(data) {
            return this._shiftData(data);
        },
        
        _shiftData: function(data) {
-            this._shiftRight(data, this._upperLeftPointIndex(data));	
-		    return data;
+           this._shiftRight(data, this._upperLeftPointIndex(data));
+           return data;
        },
        
        _shiftRight: function(data, shifts) {
            for (var i = 0; i < shifts; i++) {
-				var point = data.shift();
-				data.push(point);
-			}
+                var point = data.shift();
+                data.push(point);
+            }
        },
        
        _upperLeftPointIndex: function(data) {
             var x = Infinity;
-			var y = -Infinity;
-			var index = -1;
-			
-			for (var i = 0; i < data.length; i++ ) {
-				var point = data[i];
-				if (point.y < y || (point.y == y && point.x < x)) {
-					x = point.x;
-					y = point.y;
-					index = i;
-				}
-			}
-			
-			return index;
+            var y = Infinity;
+            var index = -1;
+            
+            for (var i = 0; i < data.length; i++ ) {
+                var point = data[i];
+                if (point.y < y || (point.y == y && point.x < x)) {
+                    x = point.x;
+                    y = point.y;
+                    index = i;
+                }
+            }
+            return index;
        }
        
    });
