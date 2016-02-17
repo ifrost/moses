3381c1456281dd02d88223819b8896236624b4c2
Piotr Jamroz <pm.jamroz@gmail.com>
2016-02-17 01:13:49 +0000

    Simple build tester

--- build/main.js
+++ build/main.js
@@ -1,24 +1,24 @@
 define(function(require) {
 
     var DefaultMosesAlgorithm = require('../js/algorithm/default-moses-algorithm');
+    var PatternCollectionAlgorithm = require('../js/algorithm/pattern-collection-algorithm');
     var MosesFit = require('../js/algorithm/moses-fit');
     var ShiftedPointsMosesAlgorithm = require('../js/algorithm/shifted-points-moses-algorithm');
     var ReversedMosesAlgorithm = require('../js/algorithm/reversed-moses-algorithm');
-    var PatternCollectionAlgorithm = require('../js/algorithm/pattern-collection-algorithm');
-    var Match = require('../js/model/match');
     var MosesPatterns = require('../js/model/moses-patterns');
-    var PatternFactory = require('../js/model/pattern-factory');
-    var Point = require('../js/model/point');
+    var Match = require('../js/model/match');
     var PatternCollection = require('../js/model/pattern-collection');
-    var Pattern = require('../js/model/pattern');
+    var PatternFactory = require('../js/model/pattern-factory');
     var RecognitionData = require('../js/model/recognition-data');
+    var Point = require('../js/model/point');
+    var Pattern = require('../js/model/pattern');
     var DefaultRecogniser = require('../js/recogniser/default-recogniser');
     var DistanceSampler = require('../js/sampler/distance-sampler');
-    var Math = require('../js/util/math');
     var DomSampler = require('../js/sampler/dom-sampler');
     var Sampler = require('../js/sampler/sampler');
     var TimeSampler = require('../js/sampler/time-sampler');
+    var Math = require('../js/util/math');
 
-    return {"algorithm":{"DefaultMosesAlgorithm":DefaultMosesAlgorithm,"MosesFit":MosesFit,"ShiftedPointsMosesAlgorithm":ShiftedPointsMosesAlgorithm,"ReversedMosesAlgorithm":ReversedMosesAlgorithm,"PatternCollectionAlgorithm":PatternCollectionAlgorithm},"model":{"Match":Match,"MosesPatterns":MosesPatterns,"PatternFactory":PatternFactory,"Point":Point,"PatternCollection":PatternCollection,"Pattern":Pattern,"RecognitionData":RecognitionData},"recogniser":{"DefaultRecogniser":DefaultRecogniser},"sampler":{"DistanceSampler":DistanceSampler,"DomSampler":DomSampler,"Sampler":Sampler,"TimeSampler":TimeSampler},"util":{"Math":Math}}
+    return {"algorithm":{"DefaultMosesAlgorithm":DefaultMosesAlgorithm,"PatternCollectionAlgorithm":PatternCollectionAlgorithm,"MosesFit":MosesFit,"ShiftedPointsMosesAlgorithm":ShiftedPointsMosesAlgorithm,"ReversedMosesAlgorithm":ReversedMosesAlgorithm},"model":{"MosesPatterns":MosesPatterns,"Match":Match,"PatternCollection":PatternCollection,"PatternFactory":PatternFactory,"RecognitionData":RecognitionData,"Point":Point,"Pattern":Pattern},"recogniser":{"DefaultRecogniser":DefaultRecogniser},"sampler":{"DistanceSampler":DistanceSampler,"DomSampler":DomSampler,"Sampler":Sampler,"TimeSampler":TimeSampler},"util":{"Math":Math}}
 
 });
