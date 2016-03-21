dd562a71c423bb3dfd726c97af3bd09b3b1c2e99
Piotr Jamroz <pm.jamroz@gmail.com>
2016-03-21 19:47:28 +0000

    0.10.2

--- build/main.js
+++ build/main.js
@@ -4,22 +4,22 @@ define(function(require) {
     var DefaultMosesAlgorithm = require('../js/algorithm/default-moses-algorithm');
     var MosesFit = require('../js/algorithm/moses-fit');
     var PatternCollectionAlgorithm = require('../js/algorithm/pattern-collection-algorithm');
-    var ReversedMosesAlgorithm = require('../js/algorithm/reversed-moses-algorithm');
     var ShiftedPointsMosesAlgorithm = require('../js/algorithm/shifted-points-moses-algorithm');
+    var ReversedMosesAlgorithm = require('../js/algorithm/reversed-moses-algorithm');
+    var Match = require('../js/model/match');
+    var MosesPatterns = require('../js/model/moses-patterns');
+    var PatternFactory = require('../js/model/pattern-factory');
+    var PatternCollection = require('../js/model/pattern-collection');
+    var Point = require('../js/model/point');
+    var Pattern = require('../js/model/pattern');
+    var RecognitionData = require('../js/model/recognition-data');
+    var DefaultRecogniser = require('../js/recogniser/default-recogniser');
     var DistanceSampler = require('../js/sampler/distance-sampler');
     var DomSampler = require('../js/sampler/dom-sampler');
     var Sampler = require('../js/sampler/sampler');
     var TimeSampler = require('../js/sampler/time-sampler');
     var Math = require('../js/util/math');
-    var Match = require('../js/model/match');
-    var MosesPatterns = require('../js/model/moses-patterns');
-    var PatternFactory = require('../js/model/pattern-factory');
-    var PatternCollection = require('../js/model/pattern-collection');
-    var Pattern = require('../js/model/pattern');
-    var Point = require('../js/model/point');
-    var RecognitionData = require('../js/model/recognition-data');
-    var DefaultRecogniser = require('../js/recogniser/default-recogniser');
 
-    return {"algorithm":{"DefaultMosesAlgorithm":DefaultMosesAlgorithm,"MosesFit":MosesFit,"PatternCollectionAlgorithm":PatternCollectionAlgorithm,"ReversedMosesAlgorithm":ReversedMosesAlgorithm,"ShiftedPointsMosesAlgorithm":ShiftedPointsMosesAlgorithm},"sampler":{"DistanceSampler":DistanceSampler,"DomSampler":DomSampler,"Sampler":Sampler,"TimeSampler":TimeSampler},"util":{"Math":Math},"model":{"Match":Match,"MosesPatterns":MosesPatterns,"PatternFactory":PatternFactory,"PatternCollection":PatternCollection,"Pattern":Pattern,"Point":Point,"RecognitionData":RecognitionData},"recogniser":{"DefaultRecogniser":DefaultRecogniser}}
+    return {"algorithm":{"DefaultMosesAlgorithm":DefaultMosesAlgorithm,"MosesFit":MosesFit,"PatternCollectionAlgorithm":PatternCollectionAlgorithm,"ShiftedPointsMosesAlgorithm":ShiftedPointsMosesAlgorithm,"ReversedMosesAlgorithm":ReversedMosesAlgorithm},"model":{"Match":Match,"MosesPatterns":MosesPatterns,"PatternFactory":PatternFactory,"PatternCollection":PatternCollection,"Point":Point,"Pattern":Pattern,"RecognitionData":RecognitionData},"recogniser":{"DefaultRecogniser":DefaultRecogniser},"sampler":{"DistanceSampler":DistanceSampler,"DomSampler":DomSampler,"Sampler":Sampler,"TimeSampler":TimeSampler},"util":{"Math":Math}}
 
 });
