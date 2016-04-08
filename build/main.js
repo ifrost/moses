4a7fea763552893d549dca4eed4ad2513d42b5a3
Piotr Jamróz <pm.jamroz@gmail.com>
2016-04-08 21:16:03 +0000

    restructure html files

--- build/main.js
+++ build/main.js
@@ -1,13 +1,14 @@
 define(function(require) {
 
     var handjs = require("../node_modules/handjs/hand.min");
-    var DefaultMosesAlgorithm = require('../js/algorithm/default-moses-algorithm');
     var MosesFit = require('../js/algorithm/moses-fit');
     var PatternCollectionAlgorithm = require('../js/algorithm/pattern-collection-algorithm');
+    var DefaultMosesAlgorithm = require('../js/algorithm/default-moses-algorithm');
     var PolygonalLineAlgorithm = require('../js/algorithm/polygonal-line-algorithm');
     var ReversedMosesAlgorithm = require('../js/algorithm/reversed-moses-algorithm');
     var ShiftedPointsMosesAlgorithm = require('../js/algorithm/shifted-points-moses-algorithm');
     var StraightLineAlgorithm = require('../js/algorithm/straight-line-algorithm');
+    var DefaultRecogniser = require('../js/recogniser/default-recogniser');
     var Match = require('../js/model/match');
     var MosesPatterns = require('../js/model/moses-patterns');
     var PatternCollection = require('../js/model/pattern-collection');
@@ -16,12 +17,11 @@ define(function(require) {
     var Point = require('../js/model/point');
     var RecognitionData = require('../js/model/recognition-data');
     var DistanceSampler = require('../js/sampler/distance-sampler');
-    var DefaultRecogniser = require('../js/recogniser/default-recogniser');
     var DomSampler = require('../js/sampler/dom-sampler');
-    var Sampler = require('../js/sampler/sampler');
     var TimeSampler = require('../js/sampler/time-sampler');
+    var Sampler = require('../js/sampler/sampler');
     var Math = require('../js/util/math');
 
-    return {"algorithm":{"DefaultMosesAlgorithm":DefaultMosesAlgorithm,"MosesFit":MosesFit,"PatternCollectionAlgorithm":PatternCollectionAlgorithm,"PolygonalLineAlgorithm":PolygonalLineAlgorithm,"ReversedMosesAlgorithm":ReversedMosesAlgorithm,"ShiftedPointsMosesAlgorithm":ShiftedPointsMosesAlgorithm,"StraightLineAlgorithm":StraightLineAlgorithm},"model":{"Match":Match,"MosesPatterns":MosesPatterns,"PatternCollection":PatternCollection,"PatternFactory":PatternFactory,"Pattern":Pattern,"Point":Point,"RecognitionData":RecognitionData},"sampler":{"DistanceSampler":DistanceSampler,"DomSampler":DomSampler,"Sampler":Sampler,"TimeSampler":TimeSampler},"recogniser":{"DefaultRecogniser":DefaultRecogniser},"util":{"Math":Math}}
+    return {"algorithm":{"MosesFit":MosesFit,"PatternCollectionAlgorithm":PatternCollectionAlgorithm,"DefaultMosesAlgorithm":DefaultMosesAlgorithm,"PolygonalLineAlgorithm":PolygonalLineAlgorithm,"ReversedMosesAlgorithm":ReversedMosesAlgorithm,"ShiftedPointsMosesAlgorithm":ShiftedPointsMosesAlgorithm,"StraightLineAlgorithm":StraightLineAlgorithm},"recogniser":{"DefaultRecogniser":DefaultRecogniser},"model":{"Match":Match,"MosesPatterns":MosesPatterns,"PatternCollection":PatternCollection,"PatternFactory":PatternFactory,"Pattern":Pattern,"Point":Point,"RecognitionData":RecognitionData},"sampler":{"DistanceSampler":DistanceSampler,"DomSampler":DomSampler,"TimeSampler":TimeSampler,"Sampler":Sampler},"util":{"Math":Math}}
 
 });
---
+++ build/main.test.js
@@ -0,0 +1,8 @@
+define(function(require) {
+
+    var handjs = require("../node_modules/handjs/hand.min");
+    var Math = require('../test/util/math.test');
+
+    return {"util":{"Math":Math}}
+
+});
