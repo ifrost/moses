ae6a2c6a1b6b2235abd590a7bfeecdb11fc91e42
Piotr Jamróz <pm.jamroz@gmail.com>
2016-04-10 15:55:23 +0000

    0.13.1

--- build/main.js
+++ build/main.js
@@ -1,33 +1,33 @@
 define(function(require) {
 
     var handjs = require("../node_modules/handjs/hand.min");
+    var DefaultRecogniser = require('../js/recogniser/default-recogniser');
+    var Directions = require('../js/model/directions');
+    var Match = require('../js/model/match');
+    var MosesPatterns = require('../js/model/moses-patterns');
+    var PatternCollection = require('../js/model/pattern-collection');
+    var PatternFactory = require('../js/model/pattern-factory');
+    var Pattern = require('../js/model/pattern');
+    var Point = require('../js/model/point');
+    var Polyline = require('../js/model/polyline');
+    var RecognitionData = require('../js/model/recognition-data');
+    var Segment = require('../js/model/segment');
+    var DistanceSampler = require('../js/sampler/distance-sampler');
+    var Sampler = require('../js/sampler/sampler');
+    var TimeSampler = require('../js/sampler/time-sampler');
+    var DomSampler = require('../js/sampler/dom-sampler');
     var DefaultMosesAlgorithm = require('../js/algorithm/default-moses-algorithm');
     var MosesFit = require('../js/algorithm/moses-fit');
     var PatternCollectionAlgorithm = require('../js/algorithm/pattern-collection-algorithm');
-    var PolygonalLineAlgorithm = require('../js/algorithm/polygonal-line-algorithm');
     var ReversedMosesAlgorithm = require('../js/algorithm/reversed-moses-algorithm');
-    var StraightLineAlgorithm = require('../js/algorithm/straight-line-algorithm');
+    var PolygonalLineAlgorithm = require('../js/algorithm/polygonal-line-algorithm');
     var ShiftedPointsMosesAlgorithm = require('../js/algorithm/shifted-points-moses-algorithm');
-    var DistanceSampler = require('../js/sampler/distance-sampler');
-    var DomSampler = require('../js/sampler/dom-sampler');
-    var Sampler = require('../js/sampler/sampler');
-    var TimeSampler = require('../js/sampler/time-sampler');
+    var StraightLineAlgorithm = require('../js/algorithm/straight-line-algorithm');
     var Array = require('../js/util/array');
-    var Direction = require('../js/util/direction');
     var Math = require('../js/util/math');
     var Segment = require('../js/util/segment');
-    var Directions = require('../js/model/directions');
-    var Match = require('../js/model/match');
-    var PatternCollection = require('../js/model/pattern-collection');
-    var MosesPatterns = require('../js/model/moses-patterns');
-    var PatternFactory = require('../js/model/pattern-factory');
-    var Polyline = require('../js/model/polyline');
-    var Pattern = require('../js/model/pattern');
-    var Segment = require('../js/model/segment');
-    var RecognitionData = require('../js/model/recognition-data');
-    var Point = require('../js/model/point');
-    var DefaultRecogniser = require('../js/recogniser/default-recogniser');
+    var Direction = require('../js/util/direction');
 
-    return {"algorithm":{"DefaultMosesAlgorithm":DefaultMosesAlgorithm,"MosesFit":MosesFit,"PatternCollectionAlgorithm":PatternCollectionAlgorithm,"PolygonalLineAlgorithm":PolygonalLineAlgorithm,"ReversedMosesAlgorithm":ReversedMosesAlgorithm,"StraightLineAlgorithm":StraightLineAlgorithm,"ShiftedPointsMosesAlgorithm":ShiftedPointsMosesAlgorithm},"sampler":{"DistanceSampler":DistanceSampler,"DomSampler":DomSampler,"Sampler":Sampler,"TimeSampler":TimeSampler},"util":{"Array":Array,"Direction":Direction,"Math":Math,"Segment":Segment},"model":{"Directions":Directions,"Match":Match,"PatternCollection":PatternCollection,"MosesPatterns":MosesPatterns,"PatternFactory":PatternFactory,"Polyline":Polyline,"Pattern":Pattern,"Segment":Segment,"RecognitionData":RecognitionData,"Point":Point},"recogniser":{"DefaultRecogniser":DefaultRecogniser}}
+    return {"recogniser":{"DefaultRecogniser":DefaultRecogniser},"model":{"Directions":Directions,"Match":Match,"MosesPatterns":MosesPatterns,"PatternCollection":PatternCollection,"PatternFactory":PatternFactory,"Pattern":Pattern,"Point":Point,"Polyline":Polyline,"RecognitionData":RecognitionData,"Segment":Segment},"sampler":{"DistanceSampler":DistanceSampler,"Sampler":Sampler,"TimeSampler":TimeSampler,"DomSampler":DomSampler},"algorithm":{"DefaultMosesAlgorithm":DefaultMosesAlgorithm,"MosesFit":MosesFit,"PatternCollectionAlgorithm":PatternCollectionAlgorithm,"ReversedMosesAlgorithm":ReversedMosesAlgorithm,"PolygonalLineAlgorithm":PolygonalLineAlgorithm,"ShiftedPointsMosesAlgorithm":ShiftedPointsMosesAlgorithm,"StraightLineAlgorithm":StraightLineAlgorithm},"util":{"Array":Array,"Math":Math,"Segment":Segment,"Direction":Direction}}
 
 });
