fe60d03577b9459477cc876c2ec5dd169996671a
Piotr Jamróz <pm.jamroz@gmail.com>
2016-04-10 15:00:42 +0000

    0.13.0

--- build/main.test.js
+++ build/main.test.js
@@ -2,12 +2,12 @@ define(function(require) {
 
     var handjs = require("../node_modules/handjs/hand.min");
     var PolygonalLineAlgorithm = require('../test/algorithm/polygonal-line-algorithm.test');
-    var PatternFactory = require('../test/model/pattern-factory.test');
-    var Point = require('../test/model/point.test');
     var Array = require('../test/util/array.test');
     var Direction = require('../test/util/direction.test');
     var Math = require('../test/util/math.test');
+    var PatternFactory = require('../test/model/pattern-factory.test');
+    var Point = require('../test/model/point.test');
 
-    return {"algorithm":{"PolygonalLineAlgorithm":PolygonalLineAlgorithm},"model":{"PatternFactory":PatternFactory,"Point":Point},"util":{"Array":Array,"Direction":Direction,"Math":Math}}
+    return {"algorithm":{"PolygonalLineAlgorithm":PolygonalLineAlgorithm},"util":{"Array":Array,"Direction":Direction,"Math":Math},"model":{"PatternFactory":PatternFactory,"Point":Point}}
 
 });
